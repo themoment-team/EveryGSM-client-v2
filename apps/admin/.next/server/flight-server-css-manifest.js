af4ad3133a7aac208ffd8b6b67b820828546f71e
예빈 <yebin060310@gmail.com>
2023-06-07 17:25:01 +0900

    Update settings

--- apps/admin/.next/server/flight-server-css-manifest.js
+++ apps/admin/.next/server/flight-server-css-manifest.js
@@ -1,6 +1,4 @@
 self.__RSC_CSS_MANIFEST={
   "cssImports": {},
-  "cssModules": {
-    "/Users/jeon-yebin/Documents/GitHub/official-gsm-front/apps/admin/src/app/page": []
-  }
+  "cssModules": {}
 }
