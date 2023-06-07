89a1a52798437a777e245f098f437777a47b7e70
예빈 <yebin060310@gmail.com>
2023-06-08 01:58:33 +0900

    Update unit

--- apps/admin/src/components/ApproveItem/style.ts
+++ apps/admin/src/components/ApproveItem/style.ts
@@ -1,46 +1,46 @@
 import styled from "@emotion/styled";
 
 export const ApproveItem = styled.div`
-  height: 72px;
-  width: 620px;
+  height: 4.5rem;
+  width: 38.75rem;
   display: flex;
   align-items: center;
   justify-content: space-between;
-  border: 1px solid #ffff;
-  border-radius: 8px;
+  border: 0.0625rem solid #ffff;
+  border-radius: 0.5rem;
   background-color: #ffff;
   &:hover {
-    border: 1px solid #dddddd;
+    border: 0.0625rem solid #dddddd;
     transition: ease-in-out 0.3s;
   }
 `;
 
 export const ProjectWrap = styled.div`
-  width: 120px;
-  height: 46px;
+  width: 7.5rem;
+  height: 2.875rem;
   display: flex;
   align-items: center;
   justify-content: space-between;
-  margin-left: 16px;
+  margin-left: 1rem;
 `;
 
 export const ProjectDescWrap = styled.div`
-  height: 46px;
+  height: 2.875rem;
   display: flex;
   justify-content: space-evenly;
   flex-direction: column;
-  margin-left: 15px;
+  margin-left: 0.9375rem;
 `;
 
 export const Title = styled.div`
-  font-size: 18px;
+  font-size: 1.125rem;
   font-weight: 700;
   letter-spacing: -0.02em;
   text-align: left;
 `;
 
 export const Creator = styled.div`
-  font-size: 12px;
+  font-size: 0.75rem;
   font-weight: 500;
   letter-spacing: -0.02em;
   text-align: left;
@@ -51,17 +51,17 @@ export const Creator = styled.div`
 `;
 
 export const Approve = styled.div`
-  width: 72px;
-  height: 25px;
+  width: 4.5rem;
+  height: 1.5625rem;
   display: flex;
   align-items: center;
   justify-content: space-around;
-  margin-right: 32px;
+  margin-right: 2rem;
   .approve {
     background-color: #ffff;
     font-style: normal;
     font-weight: 400;
-    font-size: 14px;
+    font-size: 0.875rem;
     letter-spacing: -0.02em;
     color: #00a3ff;
     cursor: pointer;
@@ -71,7 +71,7 @@ export const Approve = styled.div`
     background-color: #ffff;
     font-style: normal;
     font-weight: 400;
-    font-size: 14px;
+    font-size: 0.875rem;
     letter-spacing: -0.02em;
     color: #f01a1a;
   }
