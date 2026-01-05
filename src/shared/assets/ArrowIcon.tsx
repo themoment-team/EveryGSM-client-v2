badde48e05ffde33933259174a7c05d9056955e6
junjuny0227 <junjuny0227@gmail.com>
2026-01-05 22:44:50 +0900

    refactor: ArrowIcon 컴포넌트 가독성 개선

--- src/shared/assets/ArrowIcon.tsx
+++ src/shared/assets/ArrowIcon.tsx
@@ -3,8 +3,28 @@ interface ArrowIconProps {
   color?: string;
 }
 
-const ArrowIcon = ({ isLarge = false, color = 'white' }: ArrowIconProps) =>
-  isLarge === false ? (
+const ArrowIcon = ({ isLarge = false, color = 'white' }: ArrowIconProps) => {
+  if (isLarge) {
+    return (
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="11"
+        height="19"
+        viewBox="0 0 11 19"
+        fill="none"
+      >
+        <path
+          d="M1.5 1.5L9.5 9.5L1.5 17.5"
+          stroke={color}
+          strokeWidth="3"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+        />
+      </svg>
+    );
+  }
+
+  return (
     <svg xmlns="http://www.w3.org/2000/svg" width="6" height="10" viewBox="0 0 6 10" fill="none">
       <path
         d="M1 1L5 5L1 9"
@@ -14,16 +34,7 @@ const ArrowIcon = ({ isLarge = false, color = 'white' }: ArrowIconProps) =>
         strokeLinejoin="round"
       />
     </svg>
-  ) : (
-    <svg xmlns="http://www.w3.org/2000/svg" width="11" height="19" viewBox="0 0 11 19" fill="none">
-      <path
-        d="M1.5 1.5L9.5 9.5L1.5 17.5"
-        stroke={color}
-        strokeWidth="3"
-        strokeLinecap="round"
-        strokeLinejoin="round"
-      />
-    </svg>
   );
+};
 
 export default ArrowIcon;
