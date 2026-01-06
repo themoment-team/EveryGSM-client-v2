12488046b5549edaf05422eb842bc8506297c0c6
yeondon <s25030@gsm.hs.kr>
2026-01-06 18:10:02 +0900

    feat : 구조 설정

--- src/widgets/profilecard/ui/index.tsx
+++ src/widgets/profilecard/ui/index.tsx
@@ -1,5 +1,18 @@
+import { cn } from '@/shared/utils';
+
 const ProfileCard = () => {
-  return <div>Profile Card Component</div>;
+  return (
+    <div className={cn(`flex h-26 w-295 gap-10 border border-[#2F2F2F] bg-[#22222280] p-6`)}>
+      <div className={cn('flex gap-4')}>
+        <div></div>
+        <div></div>
+      </div>
+      <div className={cn('flex gap-4')}>
+        <div></div>
+        <div></div>
+      </div>
+    </div>
+  );
 };
 
 export default ProfileCard;
