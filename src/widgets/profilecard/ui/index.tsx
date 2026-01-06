6e3bc63fecb18dd35ba391487ce38ef4f3036dc6
yeondon <s25030@gsm.hs.kr>
2026-01-06 21:09:07 +0900

    feat : 보더 설정

--- src/widgets/profilecard/ui/index.tsx
+++ src/widgets/profilecard/ui/index.tsx
@@ -4,7 +4,7 @@ const ProfileCard = () => {
   return (
     <div
       className={cn(
-        `flex h-26 w-295 justify-between gap-10 border border-[#2F2F2F] bg-[#22222280] p-6`,
+        `flex h-26 w-295 justify-between gap-10 rounded-xl border border-[#2F2F2F] bg-[#22222280] p-6`,
       )}
     >
       <div className={cn('flex gap-4')}>
