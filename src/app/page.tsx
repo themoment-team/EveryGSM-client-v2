3454c5417cdf5d2f1c8c880553470b6f9222255e
yeondon <s25030@gsm.hs.kr>
2026-01-07 18:42:12 +0900

    feat : 날짜 출력

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,13 @@
 import { HomePage } from '@/views/home';
+import { ProfileCard } from '@/widgets/profilecard';
 
 const Home = () => {
-  return <HomePage />;
+  return (
+    <div className="flex justify-center bg-[#191919]">
+      <ProfileCard />
+      <HomePage />
+    </div>
+  );
 };
 
 export default Home;
