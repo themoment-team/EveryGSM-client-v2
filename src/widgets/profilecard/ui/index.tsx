40966e765754cd694459408d5936a99939f665a6
yeondon <s25030@gsm.hs.kr>
2026-01-07 19:52:12 +0900

    fix : cn 함수 ` 에서 '로 변경

--- src/widgets/profilecard/ui/index.tsx
+++ src/widgets/profilecard/ui/index.tsx
@@ -20,7 +20,7 @@ const ProfileCard = ({
   return (
     <div
       className={cn(
-        `flex h-26 w-295 justify-between gap-10 rounded-xl border border-[#2F2F2F] bg-[#22222280] p-6`,
+        'flex h-26 w-295 justify-between gap-10 rounded-xl border border-[#2F2F2F] bg-[#22222280] p-6',
       )}
     >
       <div className={cn('flex gap-4')}>
@@ -32,8 +32,8 @@ const ProfileCard = ({
           className={cn('h-14 w-14 rounded-full')}
         />
 
-        <div className={cn(`flex flex-col gap-2`)}>
-          <div className={cn(`flex h-6 items-center text-[1.25rem] font-semibold text-white`)}>
+        <div className={cn('flex flex-col gap-2')}>
+          <div className={cn('flex h-6 items-center text-[1.25rem] font-semibold text-white')}>
             {projectName}
           </div>
           <div className={cn('flex h-4 items-center text-[0.875rem] font-medium text-[#9A9A9A]')}>
@@ -42,10 +42,10 @@ const ProfileCard = ({
         </div>
       </div>
       <div className={cn('flex items-center gap-4')}>
-        <div className={cn(`flex text-[0.875rem] text-[#9A9A9A]`)}>{date}</div>
-        <div className={cn(`text-[1.25rem] font-semibold`)}>
-          <span className={cn(`text-[#FFF]`)}>요청 상태: </span>
-          <span className={cn(requestStatus === '거절' ? `text-[#FF7C7C]` : `text-[#888]`)}>
+        <div className={cn('flex text-[0.875rem] text-[#9A9A9A]')}>{date}</div>
+        <div className={cn('text-[1.25rem] font-semibold')}>
+          <span className={cn('text-[#FFF]')}>요청 상태: </span>
+          <span className={cn(requestStatus === '거절' ? 'text-[#FF7C7C]' : 'text-[#888]')}>
             {requestStatus}
           </span>
         </div>
