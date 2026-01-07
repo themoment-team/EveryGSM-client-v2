fcaeda185a0faa9f8b5796e4204a08a413dd394c
yeondon <s25030@gsm.hs.kr>
2026-01-07 19:11:52 +0900

    feat : 메인페이지 코드 구

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,7 @@
 import { HomePage } from '@/views/home';
-import { ProfileCard } from '@/widgets/profilecard';
 
 const Home = () => {
-  return (
-    <div className="flex justify-center bg-[#191919]">
-      <ProfileCard />
-      <HomePage />
-    </div>
-  );
+  return <HomePage />;
 };
 
 export default Home;
