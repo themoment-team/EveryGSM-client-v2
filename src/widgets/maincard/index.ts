628794d4b1449befca59113266ca48666df6e37a
서연 <s25037@gsm.hs.kr>
2026-01-09 11:02:23 +0900

    feat: maincardprops 설정

--- src/widgets/maincard/index.ts
+++ src/widgets/maincard/index.ts
@@ -1,2 +1,3 @@
-export { default as MainCard } from './ui/index';
+export { default as MainCard } from './ui/projectcard';
 export { default as MainCardModal } from './ui/projectdetail';
+export type { MainCardProps } from './ui/projectcard';
