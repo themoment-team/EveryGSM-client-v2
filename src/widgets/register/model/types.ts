6bb02265b26c62bfcd59deb6d829faa13fd437ed
LeeSangHyeok0731 <s24066@gsm.hs.kr>
2026-02-07 20:35:55 +0900

    feat: register schema, type 추가

---
+++ src/widgets/register/model/types.ts
@@ -0,0 +1,25 @@
+import { z } from 'zod';
+
+export const RegisterFormSchema = z.object({
+  projectLogo: z.string().min(1, '프로젝트 로고파일을 업로드해주세요'),
+  projectTitle: z.string().min(1, '프로젝트 제목을 입력해주세요'),
+  projectTeamName: z.string().min(1, '소속 동아리 또는 팀명을 입력해주세요'),
+  projectDescription: z
+    .string()
+    .min(1, '프로젝트 설명을 입력해주세요')
+    .max(200, '프로젝트 설명은 최대 200자까지 입력할 수 있습니다.'),
+  tech: z
+    .array(z.string().min(1, '기술 스택을 입력해주세요'))
+    .max(50, '기술 스택은 최대 50개까지 추가할 수 있습니다.'),
+  githubRepository: z
+    .array(
+      z.object({
+        repoName: z.string().min(1, '깃허브 레포지토리의 제목과 URL을 입력해주세요'),
+        repoUrl: z.string().min(1, '깃허브 레포지토리의 제목과 URL을 입력해주세요'),
+      }),
+    )
+    .max(10, 'GitHub 레포지토리는 최대 10개까지 추가할 수 있습니다.'),
+  deployUrl: z.string().min(1, '프로젝트 배포 URL을 입력해주세요'),
+});
+
+export type RegisterFormSchemaType = z.infer<typeof RegisterFormSchema>;
