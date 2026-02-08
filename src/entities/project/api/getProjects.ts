c68a88f2a39b22e965d81caf47299dcf54fc2f9a
seoxeon09 <s25037@gsm.hs.kr>
2026-02-09 01:37:33 +0900

    feat: 프로젝트 조회 api 추가

---
+++ src/entities/project/api/getProjects.ts
@@ -0,0 +1,6 @@
+import { get } from '@/shared/api/http';
+import { GetProjectsResponse } from '../model/api.types';
+
+export const getProjects = async (): Promise<GetProjectsResponse> => {
+  return await get<GetProjectsResponse>('/v1/projects');
+};
