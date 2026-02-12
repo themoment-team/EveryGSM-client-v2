cc23bf29ed1808367e13308f3f2d1c01a46c35e1
seoxeon09 <s25037@gsm.hs.kr>
2026-02-12 20:07:22 +0900

    remove: 불필요한 await 키워드 삭제

--- src/entities/project/api/getProjects.ts
+++ src/entities/project/api/getProjects.ts
@@ -3,5 +3,5 @@ import { get } from '@/shared/api/http';
 import { GetProjectsResponse } from '../model/api.types';
 
 export const getProjects = async (): Promise<GetProjectsResponse> => {
-  return await get<GetProjectsResponse>('/v1/projects');
+  return get<GetProjectsResponse>('/v1/projects');
 };
