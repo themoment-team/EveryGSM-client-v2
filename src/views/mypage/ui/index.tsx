3cfd7a521859559e74a4e0e80676cdde25dcdec3
yeondon <s25030@gsm.hs.kr>
2026-02-14 06:11:30 +0900

    feat : 공통 스타일 적용

--- src/views/mypage/ui/index.tsx
+++ src/views/mypage/ui/index.tsx
@@ -4,7 +4,9 @@ import { LikeProjects, RegisteredProjects } from '@/widgets/mypage';
 const MyPage = () => {
   return (
     <div
-      className={cn('flex min-h-[calc(100vh-72px)] flex-col items-center gap-y-4 bg-[#191919] p-4')}
+      className={cn(
+        'flex min-h-[calc(100vh-72px)] flex-col items-center gap-y-4 bg-[#191919] p-4 text-[2.25rem] font-bold text-white',
+      )}
     >
       <LikeProjects />
       <RegisteredProjects />
