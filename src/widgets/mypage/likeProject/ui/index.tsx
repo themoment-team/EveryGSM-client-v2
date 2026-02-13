e1e0dfa0b32f90e6b52e267b4f77104cb8d36d42
yeondon <s25030@gsm.hs.kr>
2026-02-14 05:18:43 +0900

    feat : 타이틀 추가

--- src/widgets/mypage/likeProject/ui/index.tsx
+++ src/widgets/mypage/likeProject/ui/index.tsx
@@ -1,2 +1,10 @@
-const LikeProjects = () => {};
+import { cn } from '@/shared/utils';
+
+const LikeProjects = () => {
+  return (
+    <div className={cn('text-[2.25rem] font-bold text-white')}>
+      <span className={cn('text-[#FC335A]')}>김유찬</span> 님이 좋아요한 프로젝트
+    </div>
+  );
+};
 export default LikeProjects;
