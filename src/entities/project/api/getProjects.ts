43cd4e3b971cdfd0ac7d96796b9c074a9f9df23f
seoxeon09 <s25037@gsm.hs.kr>
2026-02-14 16:12:05 +0900

    refactor: 컨벤션에 맞게 API URL 객체화

--- src/entities/project/api/getProjects.ts
+++ src/entities/project/api/getProjects.ts
@@ -1,7 +1,8 @@
 import { get } from '@/shared/api/http';
+import { API_URLS } from '@/shared/api/apiUrls';
 
 import { GetProjectsResponseType } from '../model/types';
 
 export const getProjects = async (): Promise<GetProjectsResponseType> => {
-  return get<GetProjectsResponseType>('/v1/projects');
+  return get<GetProjectsResponseType>(API_URLS.projects.list);
 };
--- src/entities/project/api/toggleProjectLike.ts
+++ src/entities/project/api/toggleProjectLike.ts
@@ -1,7 +1,8 @@
 import { post } from '@/shared/api/http';
+import { API_URLS } from '@/shared/api/apiUrls';
 
 import type { ProjectType } from '../model/types';
 
 export const toggleProjectLike = (projectId: number) => {
-  return post<ProjectType>(`/v1/projects/like/${projectId}`);
+  return post<ProjectType>(API_URLS.projects.like(projectId));
 };
