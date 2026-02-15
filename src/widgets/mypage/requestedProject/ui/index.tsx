e76f74e29d331e68aa3a55de55fca92c48341b29
yeondon <s25030@gsm.hs.kr>
2026-02-15 20:47:47 +0900

    feat : requestedProject  퍼블리싱

--- src/widgets/mypage/requestedProject/ui/index.tsx
+++ src/widgets/mypage/requestedProject/ui/index.tsx
@@ -1,3 +1,13 @@
-const RequestedProjects = () => {};
+import { cn } from '@/shared/utils';
+
+const RequestedProjects = () => {
+  return (
+    <div className={cn('flex w-[73.75rem] flex-col gap-[2.5rem]')}>
+      <div>
+        <span className={cn('text-[#FC335A]')}>김유찬</span> 님이 등록 요청한 프로젝트
+      </div>
+    </div>
+  );
+};
 
 export default RequestedProjects;
