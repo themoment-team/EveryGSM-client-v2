e01be7fb138ea335bbfacd16455f5cb02bd4856d
yeondon <s25030@gsm.hs.kr>
2026-02-15 14:06:29 +0900

    feat : registeredProject 퍼블리싱

--- src/widgets/mypage/registeredProject/ui/index.tsx
+++ src/widgets/mypage/registeredProject/ui/index.tsx
@@ -1,4 +1,12 @@
+import { cn } from '@/shared/utils';
+
 const RegisteredProjects = () => {
-  return <div>RegisteredProjects</div>;
+  return (
+    <div className={cn('flex w-[73.75rem] flex-col gap-[2.5rem]')}>
+      <div>
+        <span className={cn('text-[#FC335A]')}>김유찬</span> 님이 등록하신 프로젝트
+      </div>
+    </div>
+  );
 };
 export default RegisteredProjects;
