eee6487835b82a15809599a1955e55a23842dd36
yeondon <s25030@gsm.hs.kr>
2026-02-20 15:59:52 +0900

    feat : my URL 추가

--- src/shared/api/apiUrls.ts
+++ src/shared/api/apiUrls.ts
@@ -1,6 +1,7 @@
 export const API_URLS = {
   projects: {
     list: '/v1/projects',
+    my: '/v1/projects/my',
     like: (projectId: number) => `/v1/projects/like/${projectId}`,
   },
 } as const;
