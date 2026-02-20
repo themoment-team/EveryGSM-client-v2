2ad5f3d413951cc63f6dfb5604fa6cb2c9eb662f
yeondon <s25030@gsm.hs.kr>
2026-02-20 14:10:20 +0900

    fix : 스타일 수정

--- src/widgets/mypage/requestedProject/ui/index.tsx
+++ src/widgets/mypage/requestedProject/ui/index.tsx
@@ -2,7 +2,7 @@ import { cn } from '@/shared/utils';
 
 const RequestedProjects = () => {
   return (
-    <div className={cn('flex w-[73.75rem] flex-col gap-[2.5rem]')}>
+    <div className={cn('flex w-295 flex-col gap-10')}>
       <div>
         <span className={cn('text-[#FC335A]')}>김유찬</span> 님이 등록 요청한 프로젝트
       </div>
