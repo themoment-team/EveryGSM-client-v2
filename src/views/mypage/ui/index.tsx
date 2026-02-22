0df061db9f23ce1c3f23f3344a592df6c56ecde2
yeondon <s25030@gsm.hs.kr>
2026-02-22 16:00:01 +0900

    feat : use client 추가

--- src/views/mypage/ui/index.tsx
+++ src/views/mypage/ui/index.tsx
@@ -1,14 +1,19 @@
+'use client';
+
 import { cn } from '@/shared/utils';
 import { LikeProjects, RegisteredProjects, RequestedProjects } from '@/widgets/mypage';
 
+import { useGetMy } from '../model';
+
 const MyPage = () => {
+  const { data } = useGetMy();
   return (
     <div
       className={cn(
         'flex min-h-[calc(100vh-72px)] flex-col items-center gap-y-4 bg-[#191919] p-4 text-[2.25rem] font-bold text-white',
       )}
     >
-      <LikeProjects />
+      <LikeProjects data={data} />
       <RegisteredProjects />
       <RequestedProjects />
     </div>
