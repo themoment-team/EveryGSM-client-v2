25d4399acc7773d285ca8c14ff6e583f19dc3f5b
yeondon <s25030@gsm.hs.kr>
2026-02-24 17:56:01 +0900

    feat : api 주소 추가

--- src/shared/api/apiUrls.ts
+++ src/shared/api/apiUrls.ts
@@ -3,5 +3,7 @@ export const API_URLS = {
     list: '/v1/projects',
     my: '/v2/projects/my',
     like: (projectId: number) => `/v1/projects/like/${projectId}`,
+    rejeckted: '/v2/projects/my/rejected',
+    pending: '/v2/projects/my/pending',
   },
 } as const;
