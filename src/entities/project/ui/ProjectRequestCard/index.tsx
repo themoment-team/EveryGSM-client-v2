70150f90471730d36c3c919677811b010d1de631
yeondon <s25030@gsm.hs.kr>
2026-02-24 20:30:14 +0900

    feat : 타입 변경에 따른 구조 수정

--- src/entities/project/ui/ProjectRequestCard/index.tsx
+++ src/entities/project/ui/ProjectRequestCard/index.tsx
@@ -1,15 +1,15 @@
 import Image from 'next/image';
 
+import type { CheckRequestStatusType, ProjectType } from '@/entities/project/model/types';
 import { cn } from '@/shared/utils';
 
-import type { ProjectRequest } from '../../model/types';
-
 interface ProjectRequestCardProps {
-  data: ProjectRequest;
+  data: ProjectType;
+  requestStatus: CheckRequestStatusType;
 }
 
-export const ProjectRequestCard = ({ data }: ProjectRequestCardProps) => {
-  const { imageSrc, projectName, teamName, date, requestStatus } = data;
+export const ProjectRequestCard = ({ data, requestStatus }: ProjectRequestCardProps) => {
+  const { logo, title, affiliation, createdAt } = data;
 
   return (
     <div
@@ -19,22 +19,22 @@ export const ProjectRequestCard = ({ data }: ProjectRequestCardProps) => {
     >
       <div className={cn('flex items-center gap-x-4')}>
         <Image
-          src={imageSrc}
+          src={logo}
           alt="프로젝트 이미지"
           width={56}
           height={56}
           className={cn('rounded-full object-cover')}
         />
         <div className={cn('flex flex-col gap-y-2')}>
-          <h3 className={cn('text-xl leading-6 font-semibold text-white')}>{projectName}</h3>
-          <p className={cn('text-sm leading-4.25 text-[#9A9A9A]')}>{teamName}</p>
+          <h3 className={cn('text-xl leading-6 font-semibold text-white')}>{title}</h3>
+          <p className={cn('text-sm leading-4.25 text-[#9A9A9A]')}>{affiliation}</p>
         </div>
       </div>
       <div className={cn('flex items-center gap-x-4')}>
-        <p className={cn('text-sm leading-4.25 text-[#9A9A9A]')}>{date}</p>
+        <p className={cn('text-sm leading-4.25 text-[#9A9A9A]')}>{createdAt}</p>
         <p className={cn('text-xl leading-6 font-semibold')}>
           <span className={cn('text-white')}>요청 상태: </span>
-          <span className={cn(requestStatus === '거절' ? 'text-[#FF7C7C]' : 'text-[#888888]')}>
+          <span className={cn(requestStatus === 'rejected' ? 'text-[#FF7C7C]' : 'text-[#888888]')}>
             {requestStatus}
           </span>
         </p>
