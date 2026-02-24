635680ddb91e06caeb2354d7110626bb61b81148
yeondon <s25030@gsm.hs.kr>
2026-02-24 19:49:52 +0900

    refactor : 상태를 부모로 이전

--- src/entities/project/ui/checkrequeststatus.tsx
+++ src/entities/project/ui/checkrequeststatus.tsx
@@ -1,21 +1,20 @@
 'use client';
 
-import React from 'react';
-
 import { cn } from '@/shared/utils';
 
 import type { CheckRequestStatusType } from '../model/types';
 
-const CheckRequestStatus = () => {
-  const [status, setStatus] = React.useState<CheckRequestStatusType>('pending');
+interface CheckRequestStatusProps {
+  status: CheckRequestStatusType;
+  onStatusChange: (status: CheckRequestStatusType) => void;
+}
 
+const CheckRequestStatus = ({ status, onStatusChange }: CheckRequestStatusProps) => {
   return (
     <div>
       <div className={cn('flex gap-6')}>
         <button
-          onClick={() => {
-            setStatus('rejected');
-          }}
+          onClick={() => onStatusChange('rejected')}
           className={cn(
             'flex cursor-pointer items-center rounded-[62.5rem] px-4 py-2 text-base font-medium',
             status === 'rejected' ? 'bg-[#333333]' : 'text-[#B0B0B0]',
@@ -24,9 +23,7 @@ const CheckRequestStatus = () => {
           요청상태:&nbsp;<span className={cn('text-[#FF7C7C]')}>거절</span>
         </button>
         <button
-          onClick={() => {
-            setStatus('pending');
-          }}
+          onClick={() => onStatusChange('pending')}
           className={cn(
             'flex cursor-pointer items-center rounded-[62.5rem] px-4 py-2 text-base font-medium',
             status === 'pending' ? 'bg-[#333333]' : 'text-[#B0B0B0]',
