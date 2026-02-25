4b6d4546c79be8f19ac2053fc5f51230a78feaad
yeondon <s25030@gsm.hs.kr>
2026-02-25 13:31:45 +0900

    feat : 퍼블리싱 완료

--- src/widgets/mypage/requestedProject/ui/index.tsx
+++ src/widgets/mypage/requestedProject/ui/index.tsx
@@ -1,7 +1,24 @@
-import { CheckRequestStatus } from '@/entities/project';
+'use client';
+
+import React from 'react';
+
+import {
+  CheckRequestStatus,
+  CheckRequestStatusType,
+  ProjectRequestCard,
+  projectMockList,
+} from '@/entities/project';
 import { cn } from '@/shared/utils';
+import { useGetPending, useGetRejected } from '@/widgets/mypage/requestedProject/model';
 
 const RequestedProjects = () => {
+  const [status, setStatus] = React.useState<CheckRequestStatusType>('pending');
+
+  const { data: pendingData } = useGetPending();
+  const { data: rejectedData } = useGetRejected();
+
+  const data = status === 'pending' ? pendingData : rejectedData;
+
   return (
     <div className={cn('flex w-295 flex-col gap-10')}>
       <div className={cn('flex justify-between')}>
@@ -9,8 +26,14 @@ const RequestedProjects = () => {
           <span className={cn('text-[#FC335A]')}>김유찬</span> 님이 등록 요청한 프로젝트
         </div>
         <div>
-          <CheckRequestStatus />
+          <CheckRequestStatus status={status} onStatusChange={setStatus} />
         </div>
+      </div>
+
+      <div className={cn('flex flex-col gap-4')}>
+        {projectMockList?.map((project) => (
+          <ProjectRequestCard key={project.projectId} data={project} requestStatus={status} />
+        ))}
       </div>
     </div>
   );
