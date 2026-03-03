d779d5710421810bc481ca538ce88da4af710e89
yeondon <s25030@gsm.hs.kr>
2026-03-03 12:19:37 +0900

    feat : props 설정

--- src/entities/project/ui/ProjectRequestcontent.tsx
+++ src/entities/project/ui/ProjectRequestcontent.tsx
@@ -1,4 +1,12 @@
-const ProjectRequestcontent = () => {
+import type { CheckRequestStatusType, ProjectType } from '@/entities/project/model/types';
+import { cn } from '@/shared/utils';
+
+const ProjectRequestcontent = ({ data, requestStatus }: ProjectRequestCardProps) => {
+  interface ProjectRequestCardProps {
+    data: ProjectType;
+    requestStatus: CheckRequestStatusType;
+  }
+
   return <div>ProjectRequestcontent</div>;
 };
 
