8a850e9dbace1bbd8ce3851954075466e74c7088
yeondon <s25030@gsm.hs.kr>
2026-03-03 12:10:03 +0900

    fix : 대소문자 변경 임시 이름 변경

--- src/entities/project/index.ts
+++ src/entities/project/index.ts
@@ -4,4 +4,4 @@ export * from './model/useGetProjects';
 export { default as ProjectCard } from './ui/ProjectCard';
 export { default as ProjectDetailModal } from './ui/ProjectDetailModal';
 export { default as ProjectRequestCard } from './ui/ProjectRequestCard';
-export { default as CheckRequestStatus } from './ui/checkrequeststatus';
+export { default as CheckRequestStatus } from './ui/ㅁㄴㅇㄹ';
