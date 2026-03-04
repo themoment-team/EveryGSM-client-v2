943f2f8291ee98c4cdec595abc60995aa94093bc
yeondon <s25030@gsm.hs.kr>
2026-03-04 14:54:08 +0900

    feat : 거절 사유 퍼블리싱

--- src/entities/project/ui/ProjectRequestcontent.tsx
+++ src/entities/project/ui/ProjectRequestcontent.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import type { CheckRequestStatusType, ProjectType } from '@/entities/project/model/types';
-import { useModalStore } from '@/shared/stores';
+import { useRequestModalStore } from '@/shared/stores';
 import { cn } from '@/shared/utils';
 
 interface ProjectRequestCardProps {
@@ -10,9 +10,34 @@ interface ProjectRequestCardProps {
 }
 
 const ProjectRequestcontent = ({ data, requestStatus }: ProjectRequestCardProps) => {
-  const { closeModal } = useModalStore();
+  const { closeRequestModal } = useRequestModalStore();
 
-  return <div>ProjectRequestcontent</div>;
+  return (
+    <div className={cn('flex flex-col items-center justify-center gap-4')}>
+      {requestStatus === 'rejected' && (
+        <div
+          className={cn(
+            'flex w-212 flex-col gap-3 rounded-xl border border-[#2F2F2F] bg-[rgba(34,34,34,0.5)] p-6',
+          )}
+        >
+          <div
+            className={cn(
+              'text-[2.25rem] leading-[120%] font-bold tracking-[-0.045rem] text-[#FF7C7C]',
+            )}
+          >
+            거절사유
+          </div>
+          <div
+            className={cn(
+              'text-base leading-[120%] font-medium tracking-[-0.03rem] wrap-break-word whitespace-pre-wrap text-white',
+            )}
+          >
+            {data.reason}
+          </div>
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default ProjectRequestcontent;
--- src/shared/ui/RequestModalContainer/index.tsx
+++ src/shared/ui/RequestModalContainer/index.tsx
@@ -2,23 +2,17 @@
 
 import { useRequestModalStore } from '@/shared/stores';
 import { cn } from '@/shared/utils';
+import { Header } from '@/widgets/header';
 
 const RequestModalContainer = () => {
-  const { isOpen, content, closeRequestModal } = useRequestModalStore();
+  const { isOpen, content } = useRequestModalStore();
 
   if (!isOpen || !content) return null;
 
   return (
-    <div
-      className={cn('fixed inset-0 flex items-center justify-center bg-[#191919] px-4')}
-      onClick={closeRequestModal}
-    >
-      <div
-        className={cn('flex w-full items-center justify-center')}
-        onClick={(e) => e.stopPropagation()}
-      >
-        {content}
-      </div>
+    <div className={cn('fixed inset-0 overflow-y-auto bg-[#191919]')}>
+      <Header />
+      <div className={cn('min-h-screen')}>{content}</div>
     </div>
   );
 };
