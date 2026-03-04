89976caf880769acd6badba4ee81b76b0ddc3612
yeondon <s25030@gsm.hs.kr>
2026-03-04 15:57:01 +0900

    feat : 컴포넌트 구조 설정

--- src/entities/project/ui/ProjectRequestcontent.tsx
+++ src/entities/project/ui/ProjectRequestcontent.tsx
@@ -1,4 +1,5 @@
 'use client';
+import Image from 'next/image';
 
 import type { CheckRequestStatusType, ProjectType } from '@/entities/project/model/types';
 import { useRequestModalStore } from '@/shared/stores';
@@ -36,6 +37,98 @@ const ProjectRequestcontent = ({ data, requestStatus }: ProjectRequestCardProps)
           </div>
         </div>
       )}
+      <div
+        className={cn(
+          'flex w-212 flex-col gap-[2.19rem] rounded-xl border border-[#2F2F2F] bg-[rgba(34,34,34,0.5)] p-6',
+        )}
+      >
+        <div
+          className={cn('text-[2.25rem] leading-[120%] font-bold tracking-[-0.045rem] text-white')}
+        >
+          프로젝트 등록 요청 작성 내용
+        </div>
+        <div>
+          <p className={cn('text-xl leading-6 font-semibold')}>
+            <span className={cn('text-white')}>요청 상태: </span>
+            <span
+              className={cn(requestStatus === 'rejected' ? 'text-[#FF7C7C]' : 'text-[#888888]')}
+            >
+              {requestStatus === 'rejected' ? '거절' : '확인 중'}
+            </span>
+          </p>
+        </div>
+        <div className={cn('flex w-full flex-col gap-9')}>
+          <div className={cn('flex flex-col gap-3')}>
+            <div
+              className={cn('text-base leading-[120%] font-medium tracking-[-0.02rem] text-[#DDD]')}
+            >
+              프로젝트 로고
+            </div>
+
+            <Image
+              src={data.logo}
+              alt="프로젝트 이미지"
+              width={56}
+              height={56}
+              className={cn('rounded-full object-cover')}
+            />
+          </div>
+          <div className={cn('flex flex-col gap-3')}>
+            <div
+              className={cn(
+                'text-base leading-[120%] font-medium tracking-[-0.02rem] text-[#BBBBBB]',
+              )}
+            >
+              프로젝트 제목
+            </div>
+          </div>
+          <div className={cn('flex flex-col gap-3')}>
+            <div
+              className={cn(
+                'text-base leading-[120%] font-medium tracking-[-0.02rem] text-[#BBBBBB]',
+              )}
+            >
+              소속 동아리 또는 팀명
+            </div>
+          </div>
+          <div className={cn('flex flex-col gap-3')}>
+            <div
+              className={cn(
+                'text-base leading-[120%] font-medium tracking-[-0.02rem] text-[#BBBBBB]',
+              )}
+            >
+              프로젝트 설명
+            </div>
+          </div>
+          <div className={cn('flex flex-col gap-3')}>
+            <div
+              className={cn(
+                'text-base leading-[120%] font-medium tracking-[-0.02rem] text-[#BBBBBB]',
+              )}
+            >
+              기술 스택
+            </div>
+          </div>
+          <div className={cn('flex flex-col gap-3')}>
+            <div
+              className={cn(
+                'text-base leading-[120%] font-medium tracking-[-0.02rem] text-[#DDDDDD]',
+              )}
+            >
+              깃허브 레포지토리
+            </div>
+          </div>
+          <div className={cn('flex flex-col gap-3')}>
+            <div
+              className={cn(
+                'text-base leading-[120%] font-medium tracking-[-0.02rem] text-[#BBBBBB]',
+              )}
+            >
+              프로젝트 배포 URL
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
