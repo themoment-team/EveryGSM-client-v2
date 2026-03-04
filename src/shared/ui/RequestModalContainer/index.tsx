f03f129f537e97a5a55e7e5125db91468cae7b86
yeondon <s25030@gsm.hs.kr>
2026-03-04 12:24:42 +0900

    fix : 모달 겹치는 현상 해결

--- src/shared/ui/RequestModalContainer/index.tsx
+++ src/shared/ui/RequestModalContainer/index.tsx
@@ -1,17 +1,17 @@
 'use client';
 
-import { useModalStore } from '@/shared/stores';
+import { useRequestModalStore } from '@/shared/stores';
 import { cn } from '@/shared/utils';
 
 const RequestModalContainer = () => {
-  const { isOpen, content, closeModal } = useModalStore();
+  const { isOpen, content, closeRequestModal } = useRequestModalStore();
 
   if (!isOpen || !content) return null;
 
   return (
     <div
       className={cn('fixed inset-0 flex items-center justify-center bg-[#191919] px-4')}
-      onClick={closeModal}
+      onClick={closeRequestModal}
     >
       <div
         className={cn('flex w-full items-center justify-center')}
