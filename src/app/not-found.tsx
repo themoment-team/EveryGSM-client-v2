3e99bd023e9630850f30aeb4f0162221129d802a
seoxeon09 <s25037@gsm.hs.kr>
2026-03-26 08:02:56 +0900

    feat: 404 페이지 서버 컴포넌트로 구현

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,5 +1,29 @@
-import { NotFound } from '@/shared/ui';
+import Link from 'next/link';
 
-export default function NotFoundPage() {
-  return <NotFound />;
-}
+import { ArrowIcon } from '@/shared/assets';
+import { cn } from '@/shared/utils';
+
+const NotFound = () => {
+  return (
+    <div
+      className={cn(
+        'flex h-screen flex-col items-center justify-center gap-6 overflow-hidden bg-[#191919] px-4 text-center',
+      )}
+    >
+      <h1 className={cn('text-[8rem] font-bold text-[#FC335A]')}>404</h1>
+      <p className={cn('text-[2.25rem] font-bold text-[#FFFFFF]')}>잘못된 경로입니다</p>
+      <p className={cn('text-[1rem] font-medium text-[#FFFFFF]')}>
+        존재하지 않는 주소를 입력하셨거나, <br />
+        요청하신 페이지의 주소가 변경, 삭제되어 찾을 수 없습니다.
+      </p>
+      <Link
+        href="/"
+        className={cn('flex items-center gap-4 text-[1rem] font-medium text-[#FFFFFF]')}
+      >
+        메인페이지로 <ArrowIcon />
+      </Link>
+    </div>
+  );
+};
+
+export default NotFound;
--- src/shared/ui/index.ts
+++ src/shared/ui/index.ts
@@ -1,2 +1,1 @@
 export { default as ModalContainer } from './ModalContainer';
-export { default as NotFound } from './NotFound';
