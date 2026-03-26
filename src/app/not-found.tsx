deadb23dc0ca59170740c82884753454ffb89f5d
seoxeon09 <s25037@gsm.hs.kr>
2026-03-26 11:13:35 +0900

    fix: 404 페이지 텍스트 스타일 Figma 기준으로 조정

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -10,15 +10,23 @@ const NotFound = () => {
         'flex h-[calc(100vh-72px)] flex-col items-center justify-center gap-6 bg-[#191919]',
       )}
     >
-      <h1 className={cn('text-[8rem] font-bold text-[#FC335A]')}>404</h1>
-      <p className={cn('text-[2.25rem] font-bold text-[#FFFFFF]')}>잘못된 경로입니다</p>
-      <p className={cn('text-[1rem] font-medium text-[#FFFFFF]')}>
+      <h1 className={cn('text-9xl leading-[9.6rem] font-bold tracking-[-0.16rem] text-[#FC335A]')}>
+        404
+      </h1>
+      <p
+        className={cn(
+          'text-center text-2xl leading-[2.7rem] font-bold tracking-[-0.045rem] text-[#FFF]',
+        )}
+      >
+        잘못된 경로입니다
+      </p>
+      <p className={cn('text-center text-base leading-6 font-medium text-[#DDD]')}>
         존재하지 않는 주소를 입력하셨거나, <br />
         요청하신 페이지의 주소가 변경, 삭제되어 찾을 수 없습니다.
       </p>
       <Link
         href="/"
-        className={cn('flex items-center gap-4 text-[1rem] font-medium text-[#FFFFFF]')}
+        className={cn('flex items-center gap-4 text-base leading-6 font-medium text-[#DDD]')}
       >
         메인페이지로 <ArrowIcon />
       </Link>
