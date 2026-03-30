9b3a7d8ed1c0ab090ab777ec4429819fa504c1a4
yeondon <s25030@gsm.hs.kr>
2026-03-30 21:03:59 +0900

    fix : 주석처리로 인한 prop 타입에러 수정

--- src/views/home/ui/HomePage.tsx
+++ src/views/home/ui/HomePage.tsx
@@ -4,11 +4,17 @@ import { useEffect } from 'react';
 
 import { toast } from 'sonner';
 
-import { useGetUserInfo, UserInfoResponseType } from '@/entities/auth';
+import {
+  //  useGetUserInfo,
+  UserInfoResponseType,
+} from '@/entities/auth';
 import { ProjectsListResponseType, useGetProjects } from '@/entities/project';
-import { COOKIE_KEYS } from '@/shared/constants';
+// import { COOKIE_KEYS } from '@/shared/constants';
 import { useHandleErrorQueryToast } from '@/shared/hooks';
-import { cn, getCookie } from '@/shared/utils';
+import {
+  cn,
+  //  getCookie
+} from '@/shared/utils';
 import { HeroSection } from '@/widgets/hero-section';
 import { ProjectList } from '@/widgets/project-list';
 
@@ -17,16 +23,19 @@ interface HomePageProps {
   initialProjectsData?: ProjectsListResponseType;
 }
 
-const HomePage = ({ initialUserInfoData, initialProjectsData }: HomePageProps) => {
-  const hasAccessToken = Boolean(getCookie(COOKIE_KEYS.ACCESS_TOKEN));
+const HomePage = ({
+  // initialUserInfoData,
+  initialProjectsData,
+}: HomePageProps) => {
+  // const hasAccessToken = Boolean(getCookie(COOKIE_KEYS.ACCESS_TOKEN));
 
-  const { data: userInfoData } = useGetUserInfo({
-    initialData: initialUserInfoData,
-    enabled: hasAccessToken,
-  });
+  // const { data: userInfoData } = useGetUserInfo({
+  //   initialData: initialUserInfoData,
+  //   enabled: hasAccessToken,
+  // });
   const { data: projectsData } = useGetProjects({ initialData: initialProjectsData });
 
-  const isLoggedIn = Boolean(userInfoData?.data?.id);
+  // const isLoggedIn = Boolean(userInfoData?.data?.id);
   const projects = projectsData?.data.projects ?? [];
 
   useHandleErrorQueryToast({
@@ -53,7 +62,10 @@ const HomePage = ({ initialUserInfoData, initialProjectsData }: HomePageProps) =
           title={`GSM의 프로젝트를 한 눈에,\nEveryGSM에서 간편하게 확인해보세요!`}
           description={`EveryGSM은 GSM의 프로젝트들을 한 곳에 모아 트래픽을 집중시키기 위한 서비스로,\n사용자가 GSM의 사이트를 보다 쉽게 방문하기 위해 만들어졌습니다.`}
         />
-        <ProjectList projects={projects} showRegisterCard={isLoggedIn} />
+        <ProjectList
+          projects={projects}
+          // showRegisterCard={isLoggedIn}
+        />
       </div>
     </main>
   );
