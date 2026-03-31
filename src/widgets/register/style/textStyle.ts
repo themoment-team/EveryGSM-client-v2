cd6d7593ecf5fdabadf0a580dbf7e80a8c28a458
LeeSangHyeok0731 <s24066@gsm.hs.kr>
2026-03-31 12:24:58 +0900

    fix: text 스타일 구조 개선

--- src/widgets/register/style/textStyle.ts
+++ src/widgets/register/style/textStyle.ts
@@ -1,10 +1,10 @@
 import { cn } from '@/shared/utils';
 
-const textStyle = cn('text-white text-base font-medium leading-[1.2] tracking-[-0.02rem]');
-const inputTextStyle = cn('text-[#9A9A9A] text-base font-medium leading-[1.2] tracking-[-0.03rem]');
-const errorTextStyle = cn('text-[#FF7C7C] text-base font-medium leading-[1.2] tracking-[-0.02rem]');
-const annotationStyle = cn(
-  'text-[#6A6A6A] text-base font-medium leading-[1.2] tracking-[-0.02rem]',
-);
+const baseTextStyle = 'text-base font-medium leading-[1.2]';
+
+const textStyle = cn(baseTextStyle, 'text-white tracking-[-0.02rem]');
+const inputTextStyle = cn(baseTextStyle, 'text-[#9A9A9A] tracking-[-0.03rem]');
+const errorTextStyle = cn(baseTextStyle, 'text-[#FF7C7C] tracking-[-0.02rem]');
+const annotationStyle = cn(baseTextStyle, 'text-[#6A6A6A] tracking-[-0.02rem]');
 
 export { annotationStyle, errorTextStyle, inputTextStyle, textStyle };
